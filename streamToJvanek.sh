bdba8b3297305c0da4962d6ac8c0d269863c5c5f
Jiri <jvanek@redhat.com>
2023-01-14 16:38:04 +0100

    nice to sound send and some improvements

--- streamToJvanek.sh
+++ streamToJvanek.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #arecord --device=hw:1,0  -f dat | ssh -C jvanek@jvanek.local  aplay -f dat
 #arecord --device=hw:1,0  -f dat | ssh -C jvanek@jvanek.local  cat
-arecord --device=hw:1,0 --format S16_LE --rate 44100 -V mono -c1  | ssh -C jvanek@jvanek.local  aplay
+nice -n 19 arecord --device=hw:1,0 --format S16_LE --rate 44100 -V mono -c1  | ssh -C jvanek@jvanek.local  aplay
 
 
